f3bd03b6824c8d9dc9474effb66a0c269fdb4099
FatimeyiZahra <fatimeyizahrai@gmail.com>
2020-07-31 23:31:43 +0400

    qr code generate

---
+++ qr/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pic = New System.Windows.Forms.PictureBox()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        CType(Me.pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pic
+        '
+        Me.pic.BackColor = System.Drawing.SystemColors.Window
+        Me.pic.Location = New System.Drawing.Point(26, 31)
+        Me.pic.Name = "pic"
+        Me.pic.Size = New System.Drawing.Size(946, 524)
+        Me.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic.TabIndex = 0
+        Me.pic.TabStop = False
+        '
+        'txtInput
+        '
+        Me.txtInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtInput.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInput.Location = New System.Drawing.Point(83, 589)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(566, 104)
+        Me.txtInput.TabIndex = 1
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(729, 627)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(150, 55)
+        Me.btnGenerate.TabIndex = 2
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(1061, 723)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.pic)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "QR Code"
+        CType(Me.pic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pic As PictureBox
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnGenerate As Button
+End Class
